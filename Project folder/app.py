939fe8f033c11ff3fdfed1cecd11e54578f0fd9a
Vennela G <109289449+vennelag15@users.noreply.github.com>
2023-06-29 00:14:45 +0530

    Add files via upload

---
+++ Project folder/app.py
@@ -0,0 +1,19 @@
+import numpy as np
+from flask import Flask,request,render_template
+import pickle
+app=Flask(__name__)
+model=pickle.load(open('model3.pk1','rb'))
+@app.route('/')
+def home():
+     return render_template('index.html')
+
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    int_features = [float(x) for x in request.form.values()]
+    features = [np.array(int_features)]
+    prediction = model.predict(features)
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text='Sales is {}'.format(output))
+if __name__=="__main__":
+    app.run() 
